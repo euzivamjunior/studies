4af95018d996599bd86632dcd3cefa60f079807c
euzivam junior <33323361+euzivamjunior@users.noreply.github.com>
2021-04-12 15:51:14 -0300

    Update queue.h

---
+++ C++ Data Structures/queue/queue.h
@@ -0,0 +1,29 @@
+// define default type for stack object
+typedef int ItemType;
+// max number of itens on the queue
+const int max_itens = 100;
+
+class Queue{
+    private:
+    // first, last element
+    int first, last;
+    // structure pointer
+    ItemType* structure;
+
+    public:
+    // constructor
+    Queue();
+    // destructor
+    ~Queue();
+
+    // check if queue is full
+    bool is_full();
+    // check if queue is empty
+    bool is_empty();
+    // insert an element in the queue
+    void push(ItemType item);
+    // remove an element from the queue
+    ItemType pull();
+    // print all elements in the queue
+    void print();
+};
