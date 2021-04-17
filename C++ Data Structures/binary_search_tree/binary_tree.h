813af8d937a39fa674cc2bc100acefec095a7c78
euzivam junior <33323361+euzivamjunior@users.noreply.github.com>
2021-04-17 10:18:27 -0300

    Update binary_tree.h

--- C++ Data Structures/binary_search_tree/binary_tree.h
+++ C++ Data Structures/binary_search_tree/binary_tree.h
@@ -24,6 +24,12 @@ class BinarySearchTree{
     bool is_full();
     void insert(Student student);
     void remove(Student student);
+    // Node*& current_node: (Node by reference) pointer to the current node,
+    // not the current node indeed.
+    void remove_search(Student student, Node*& current_node);
+    void delete_node(Node*& current_node);
+    // (Student object by reference)
+    void get_successor(Student& student_successor, Node* temp_node);
     void search(Student& student, bool& found_element);
     void print_pre_order(Node* current_node);
     void print_in_order(Node* current_node);
