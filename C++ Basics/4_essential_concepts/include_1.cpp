c63f8d6dc8993cef9ddd85ffe60b7117e7f39bf5
euzivamjunior <euzivamjunior@hotmail.com>
2021-05-02 09:35:51 -0300

    create include 1

---
+++ C++ Basics/4_essential_concepts/include_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+    HEADER FILES:
+    https://docs.microsoft.com/pt-br/cpp/cpp/header-files-cpp?view=msvc-160&viewFallbackFrom=vs-2019
+*/
+
+
+int main() {
+    cout << "#include" << endl;
+
+    return 0;
+#include "include_1.h"
